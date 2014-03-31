374596a6e96d026b831feefe4030f462266d2697
louishol <louis_hol@hotmail.com>
2014-03-31 14:05:27 +0200

    Producten pagina

--- producten/computer.php
+++ producten/computer.php
@@ -21,46 +21,47 @@ $current_url = base64_encode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQU
                 
                 <div class="products_list catalog">           
                 <?php 
-                 if($products != false) {
-		            foreach($products as $product)
-					{
-							echo '
-						  	<article>
-							<form method="post" action="../cart_update.php">
-							<div class="grid_3">
-				  			<div class="prev">
-								<a href="product_page.php?productid=' .$product["id"]. '"><img src="../' . $product['prImage'] . '" alt="Product 2" title=""></a>
-				   			</div><!-- .prev -->
-							</div><!-- .grid_3 -->
-					
-							<div class="grid_6">
-				 			 <div class="entry_content">
-								<h3 class="title">' . $product['prNaam'] . '</h3>
-		           				 <p>'. $product['prKbeschrijving'] .'</p>
-		              		</div><!-- .entry_content -->
-		                        
-		               	 	<div class="price">
-		                	€ ' . $product['prPrijs'] . ',
-				    		</div>
-				    		
-				    		Quantity <input type="text" name="product_qty" style="width: 40px">
-				    		<div class="cart">
+                if($products != false)
+                {
+		                foreach($products as $product)
+						{
+								?>
+							  	<article>
+								<form method="post" action="../cart_update.php">
+								<div class="grid_3">
+					  			<div class="prev">
+									<a href="../product_page.php?productid=<?php echo $product["id"] ?>"><img src="../<?php echo $product['prImage'] ?>" alt="Product 2" title=""></a>
+					   			</div><!-- .prev -->
+								</div><!-- .grid_3 -->
+						
+								<div class="grid_6">
+					 			 <div class="entry_content">
+									<h3 class="title"><?php echo $product['prNaam'] ?></h3>
+		               				 <p><?php echo $product['prKbeschrijving'] ?></p>
+		                  		</div><!-- .entry_content -->
+		                            
+		                   	 	<div class="price">
+		                    	€ <?php echo $product['prPrijs'] ?>
+					    		</div>
+					    		
+					    		Quantity <input type="text" name="product_qty" style="width: 40px">
+					    		<div class="cart">
 
-				   			 <button class="add_to_cart">Add To Cart</button>
+					   			 <button class="add_to_cart">Add To Cart</button>
 
-							</div><!-- .cart -->
-		        			<input type="hidden" name="product_id" value="'.$product['id'].'" />
-		        			<input type="hidden" name="type" value="add" />
-							<input type="hidden" name="return_url" value="'.$current_url.'" />
-							</div><!-- .grid_6 -->
-							<div class="clear"></div>
-							</form>
-			    			</article>';
+								</div><!-- .cart -->
+		            			<input type="hidden" name="product_id" value=<?php echo $product['id'] ?>/>
+		            			<input type="hidden" name="type" value="add" />
+								<input type="hidden" name="return_url" value= <?php echo $current_url ?> />
+								</div><!-- .grid_6 -->
+								<div class="clear"></div>
+								</form>
+				    			</article>
+				    			<?php
+						}
 					}
-				}
           
           	?>
-           <article>
     		<div class="clear"></div>
                 </div><!-- .products_list -->
                 <div class="clear"></div>
--- producten/playstation.php
+++ producten/playstation.php
@@ -26,23 +26,23 @@ $current_url = base64_encode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQU
                 {
 		                foreach($products as $product)
 						{
-								echo '
+								?>
 							  	<article>
 								<form method="post" action="../cart_update.php">
 								<div class="grid_3">
 					  			<div class="prev">
-									<a href="../product_page.php?productid=' .$product["id"]. '"><img src="../' . $product['prImage'] . '" alt="Product 2" title=""></a>
+									<a href="../product_page.php?productid=<?php echo $product["id"] ?>"><img src="../<?php echo $product['prImage'] ?>" alt="Product 2" title=""></a>
 					   			</div><!-- .prev -->
 								</div><!-- .grid_3 -->
 						
 								<div class="grid_6">
 					 			 <div class="entry_content">
-									<h3 class="title">' . $product['prNaam'] . '</h3>
-		               				 <p>'. $product['prKbeschrijving'] .'</p>
+									<h3 class="title"><?php echo $product['prNaam'] ?></h3>
+		               				 <p><?php echo $product['prKbeschrijving'] ?></p>
 		                  		</div><!-- .entry_content -->
 		                            
 		                   	 	<div class="price">
-		                    	€ ' . $product['prPrijs'] . ',
+		                    	€ <?php echo $product['prPrijs'] ?>
 					    		</div>
 					    		
 					    		Quantity <input type="text" name="product_qty" style="width: 40px">
@@ -51,19 +51,19 @@ $current_url = base64_encode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQU
 					   			 <button class="add_to_cart">Add To Cart</button>
 
 								</div><!-- .cart -->
-		            			<input type="hidden" name="product_id" value="'.$product['id'].'" />
+		            			<input type="hidden" name="product_id" value=<?php echo $product['id'] ?>/>
 		            			<input type="hidden" name="type" value="add" />
-								<input type="hidden" name="return_url" value="'.$current_url.'" />
+								<input type="hidden" name="return_url" value= <?php echo $current_url ?> />
 								</div><!-- .grid_6 -->
 								<div class="clear"></div>
 								</form>
-				    			</article>';
+				    			</article>
+				    			<?php
 						}
 					}
           
           	?>
-           <article>
-    		<div class="clear"></div>
+          	<div class="clear"></div>
                 </div><!-- .products_list -->
                 <div class="clear"></div>
 	      
