fcc16038c8019faf0972ee42ca41e4b5dcb7029c
rjong14 <rjon14@student.avans.nl>
2014-04-01 23:06:52 +0200

    takenverdeling

--- about.php
+++ about.php
@@ -3,7 +3,13 @@ include 'header.php';
 ?>
 <section id="main">
         <div class="container_12">
-        	<p>Deze webshop is gemaakt door Louis Hol en Rick de Jong, in opdracht van Avans hogeschool</p>
+                <header>
+                    <h1 class="page_title">About</h1>
+                </header>
+            <div class="text_block">
+                <br>
+                <p>Deze webshop is gemaakt door Louis Hol en Rick de Jong, in opdracht van Avans hogeschool</p>
+            </div>
         </div>
 </section>
 
